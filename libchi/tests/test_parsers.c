5c683248dbf8e447efc036b17055a3d243e7478b
Guido Flohr <guido.flohr@cantanea.com>
2019-01-13 10:53:11 +0200

    test for ambiguous file in SAN

--- libchi/tests/test_parsers.c
+++ libchi/tests/test_parsers.c
@@ -299,6 +299,45 @@ START_TEST(test_parse_move_san_promotion)
 	ck_assert_int_eq(chi_move_to(move), chi_coords2shift(5, 7));
 END_TEST
 
+START_TEST(test_parse_move_san_ambiguous_file)
+	chi_pos pos;
+	chi_move move;
+/*
+     a   b   c   d   e   f   g   h
+   +---+---+---+---+---+---+---+---+
+ 8 |   |   |   |   |   |   |   |   | En passant not possible.
+   +---+---+---+---+---+---+---+---+ White king castle: no.
+ 7 |   | R |   |   |   |   | R |   | White queen castle: no.
+   +---+---+---+---+---+---+---+---+ Black king castle: no.
+ 6 |   |   |   |   |   |   |   |   | Black queen castle: no.
+   +---+---+---+---+---+---+---+---+ Half move clock (50 moves): 0.
+ 5 |   |   | K |   |   |   |   |   | Half moves: 0.
+   +---+---+---+---+---+---+---+---+ Next move: white.
+ 4 |   |   |   |   | k |   |   |   | Material: +10.
+   +---+---+---+---+---+---+---+---+ Black has castled: no.
+ 3 |   |   |   |   |   |   |   |   | White has castled: no.
+   +---+---+---+---+---+---+---+---+
+ 2 |   |   |   |   |   |   |   |   |
+   +---+---+---+---+---+---+---+---+
+ 1 |   |   |   |   |   |   |   |   |
+   +---+---+---+---+---+---+---+---+
+     a   b   c   d   e   f   g   h
+ */
+	const char *fen = "8/1R4R1/8/2K5/4k3/8/8/8 w - - 0 1";
+	int errnum = chi_set_position(&pos, fen);
+
+	ck_assert_int_eq(errnum, 0);
+
+	errnum = chi_parse_move (&pos, &move, "Rbf7");
+	ck_assert_int_eq(errnum, 0);
+	ck_assert_int_eq(chi_move_from(move), chi_coords2shift(1, 6));
+	ck_assert_int_eq(chi_move_to(move), chi_coords2shift(5, 6));
+	errnum = chi_parse_move (&pos, &move, "Rge7+");
+	ck_assert_int_eq(errnum, 0);
+	ck_assert_int_eq(chi_move_from(move), chi_coords2shift(6, 6));
+	ck_assert_int_eq(chi_move_to(move), chi_coords2shift(4, 6));
+END_TEST
+
 Suite *
 parsers_suite(void)
 {
@@ -318,6 +357,7 @@ parsers_suite(void)
 	tcase_add_test(tc_san, test_parse_move_san_piece_capture);
 	tcase_add_test(tc_san, test_parse_move_san_ambiguous_pawn_capture);
 	tcase_add_test(tc_san, test_parse_move_san_promotion);
+	tcase_add_test(tc_san, test_parse_move_san_ambiguous_file);
 	suite_add_tcase(suite, tc_san);
 
 	return suite;
