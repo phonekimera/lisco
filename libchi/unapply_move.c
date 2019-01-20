53613d1c4777e3eb62f482b6c17a7cdb7b7a9dd2
Guido Flohr <guido.flohr@cantanea.com>
2019-01-20 17:56:05 +0200

    restore castling state after in unapply_move()

--- libchi/unapply_move.c
+++ libchi/unapply_move.c
@@ -57,10 +57,19 @@ chi_unapply_move(chi_pos *pos, chi_move move)
 
 	chi_on_move(pos) = !chi_on_move(pos);
 
-	if (chi_on_move (pos) == chi_white)
-		chi_material (pos) -= chi_move_material(move);
-	else
-		chi_material (pos) += chi_move_material(move);
+	if (chi_on_move(pos) == chi_white) {
+		chi_material(pos) -= chi_move_material(move);
+		if (pos->lost_wk_castle == pos->half_moves)
+			chi_wk_castle(pos) = 1;
+		if (pos->lost_wq_castle == pos->half_moves)
+			chi_wq_castle(pos) = 1;
+	} else {
+		chi_material(pos) += chi_move_material(move);
+		if (pos->lost_bk_castle == pos->half_moves)
+			chi_bk_castle(pos) = 1;
+		if (pos->lost_bq_castle == pos->half_moves)
+			chi_bq_castle(pos) = 1;
+	}
 
 	return chi_unmake_move(pos, move);
 }
