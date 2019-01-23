301fdd22bbb8e7fd38894470d7c4caac22111c1a
Lora Tzoneva <lora.tzoneva@cantanea.com>
2019-01-23 15:31:26 +0200

    queen side black rook capture test

--- libchi/tests/test_move_making.c
+++ libchi/tests/test_move_making.c
@@ -378,6 +378,58 @@ START_TEST(test_ks_white_rook_capture)
 END_TEST;
 
 
+START_TEST(test_qs_black_rook_capture)
+	chi_pos pos;
+
+    /*    
+    a   b   c   d   e   f   g   h
+   +---+---+---+---+---+---+---+---+
+ 8 |   |   |   |   | k |   |   | r | En passant not possible.
+   +---+---+---+---+---+---+---+---+ White king castle: no.
+ 7 |   |   |   |   |   |   |   |   | White queen castle: no.
+   +---+---+---+---+---+---+---+---+ Black king castle: yes.
+ 6 |   |   |   |   |   |   |   |   | Black queen castle: no.
+   +---+---+---+---+---+---+---+---+ Half move clock (50 moves): 0.
+ 5 |   |   |   |   |   |   |   |   | Half moves: 0.
+   +---+---+---+---+---+---+---+---+ Next move: white.
+ 4 |   |   |   |   |   |   |   |   | Material: +0.
+   +---+---+---+---+---+---+---+---+ Black has castled: no.
+ 3 |   |   |   |   |   |   |   |   | White has castled: no.
+   +---+---+---+---+---+---+---+---+
+ 2 |   |   |   |   |   |   |   | R |
+   +---+---+---+---+---+---+---+---+
+ 1 |   |   |   |   | K |   |   |   |
+   +---+---+---+---+---+---+---+---+
+     a   b   c   d   e   f   g   h
+*/
+
+    const char *fen = "r3k3/8/8/8/8/8/R7/4K3 w q - 0 2";
+	chi_move move;
+	const char *wanted;
+	char *got;
+	int errnum;
+
+    errnum = chi_set_position(&pos, fen);
+	ck_assert_int_eq(errnum, 0);
+
+	errnum = chi_parse_move(&pos, &move, "Rxa8");
+	ck_assert_int_eq(errnum, 0);
+
+	errnum = chi_apply_move(&pos, move);
+	ck_assert_int_eq(errnum, 0);
+	wanted = "R3k3/8/8/8/8/8/8/4K3 b - - 0 2";
+	got = chi_fen(&pos);
+	ck_assert_str_eq(wanted, got);
+	free(got);
+
+	errnum = chi_unapply_move(&pos, move);
+	ck_assert_int_eq(errnum, 0);
+	wanted = fen;
+	got = chi_fen(&pos);
+	ck_assert_str_eq(wanted, got);
+	free(got);
+END_TEST;
+
 Suite *
 move_making_suite(void)
 {
@@ -398,6 +450,7 @@ move_making_suite(void)
     tc_rook = tcase_create("Castling States");
     tcase_add_test(tc_rook, test_ks_black_rook_capture);
     tcase_add_test(tc_rook, test_ks_white_rook_capture);
+    tcase_add_test(tc_rook, test_qs_black_rook_capture);
     suite_add_tcase(suite, tc_rook);
 
 	return suite;
