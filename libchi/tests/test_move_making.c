99e41b6407075479fc125773705d9b96ce68cb65
Lora Tzoneva <lora.tzoneva@cantanea.com>
2019-05-30 16:38:20 +0300

    test unapplying queenside castling

--- libchi/tests/test_move_making.c
+++ libchi/tests/test_move_making.c
@@ -722,6 +722,56 @@ START_TEST(test_undo_kcastle)
 	free(positions);
 END_TEST;
 
+START_TEST(test_undo_qcastle)
+	int errnum;
+	chi_move move;
+	const char *moves[] = {
+		"d2-d4", "d7-d5",
+		"Nb1-c3", "Nb8-c6",
+		"Bc1-f4", "Bc8-f5",
+		"Qd1-d3", "Qd8-d6",
+		"O-O-O", "O-O-O"
+	};
+	size_t num_moves = sizeof moves / sizeof moves[0];
+	chi_pos *positions = xcalloc(1 + num_moves, sizeof(chi_pos));
+	size_t i;
+
+	chi_init_position(&positions[0]);
+
+	for (i = 0; i < num_moves; ++i) {
+		chi_pos *pos1, *pos2;
+		pos1 = &positions[i];
+		pos2 = &positions[i + 1];
+		chi_copy_pos(&positions[i + 1], &positions[i]);
+		errnum = chi_parse_move(&positions[i + 1], &move, moves[i]);
+		ck_assert_int_eq(errnum, 0);
+		errnum = chi_apply_move(&positions[i + 1], move);
+		ck_assert_int_eq(errnum, 0);
+	}
+
+	for (i = num_moves; i != 0; --i) {
+		chi_pos *before, *after;
+		before = &positions[i - 1];
+		after = &positions[i];
+		errnum = chi_parse_move(&positions[i - 1], &move, moves[i - 1]);
+		ck_assert_int_eq(errnum, 0);
+		errnum = chi_unapply_move(&positions[i], move);
+
+		/* Copy the insignificant book-keeping stuff before comparing.  */
+		memcpy((&positions[i])->irreversible,
+		       (&positions[i - 1])->irreversible,
+		       sizeof (&positions[i])->irreversible);
+		(&positions[i])->irreversible_count = (&positions[i - 1])->irreversible_count;
+		memcpy((&positions[i])->double_pawn_moves,
+		       (&positions[i - 1])->double_pawn_moves,
+		       sizeof (&positions[i])->irreversible);
+
+		ck_assert_int_eq(0, chi_cmp_pos(&positions[i - 1], &positions[i]));
+	}
+
+	free(positions);
+END_TEST;
+
 Suite *
 move_making_suite(void)
 {
@@ -755,6 +805,7 @@ move_making_suite(void)
 
 	tc_undo = tcase_create("Completely undo position");
 	tcase_add_test(tc_undo, test_undo_kcastle);
+	tcase_add_test(tc_undo, test_undo_qcastle);
 	suite_add_tcase(suite, tc_undo);
 
 	return suite;
