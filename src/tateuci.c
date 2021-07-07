1b4ece390f1385b0ace5d92ec86a1b4cb2092ebe
Guido Flohr <guido.flohr@cantanea.com>
2021-07-07 14:43:08 +0300

    minimal new uci based binary

---
+++ src/tateuci.c
@@ -0,0 +1,77 @@
+/* This file is part of the chess engine tate.
+ *
+ * Copyright (C) 2002-2019 cantanea EOOD.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <libchi.h>
+
+#include "basename.h"
+#include "closeout.h"
+#include "error.h"
+#include "progname.h"
+#include "xalloc.h"
+
+#include "tate.h"
+
+chi_zk_handle zk_handle;
+
+static void greeting();
+
+int
+main (argc, argv)
+int argc;
+char *argv[];
+{
+	int errnum;
+
+	set_program_name(argv[0]);
+
+	atexit(close_stdout);
+
+	greeting();
+
+	errnum = chi_zk_init(&zk_handle);
+	if (errnum)
+		error (EXIT_FAILURE,
+		       0,
+		       "Cannot initialize Zobrist key array: %s",
+		       chi_strerror (errnum));
+
+	while (1) {
+	}
+
+	return EXIT_SUCCESS;
+}
+
+static void
+greeting()
+{
+	printf("%s %s\n", basename(program_name), VERSION);
+	printf("Copyright (C) %s cantanea EOOD (http://www.cantanea.com)\n\
+License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>\n\
+This is free software: you are free to change and redistribute it.\n\
+There is NO WARRANTY, to the extent permitted by law.\n\
+",
+	       "2002-2021");
+	printf("Written by Guido Flohr.\n");
+}
