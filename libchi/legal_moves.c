96b088f746cc60be25ce2330cd4922ce563914b1
Guido Flohr <guido.flohr@cantanea.com>
2021-07-15 09:06:12 +0300

    reformatted

--- libchi/legal_moves.c
+++ libchi/legal_moves.c
@@ -23,75 +23,66 @@
 #include <libchi.h>
 
 chi_move*
-chi_legal_moves (pos, move_stack)
-     chi_pos* pos;
-     chi_move* move_stack;
+chi_legal_moves(chi_pos *pos, chi_move *move_stack)
 {
-    chi_move moves[CHI_MAX_MOVES];
-    chi_move* move_ptr;
-    chi_move* m;
+	chi_move moves[CHI_MAX_MOVES];
+	chi_move* move_ptr;
+	chi_move* m;
 
-    if (!pos || !move_stack)
-	return move_stack;
+	if (!pos || !move_stack)
+		return move_stack;
 
-    move_ptr = chi_generate_captures (pos, moves);
-    move_ptr = chi_generate_non_captures (pos, move_ptr);
+	move_ptr = chi_generate_captures(pos, moves);
+	move_ptr = chi_generate_non_captures(pos, move_ptr);
 
-    for (m = moves; m < move_ptr; ++m) {
-	chi_pos tmp_pos;
+	for (m = moves; m < move_ptr; ++m) {
+		chi_pos tmp_pos;
 
-	chi_copy_pos (&tmp_pos, pos);
+		chi_copy_pos(&tmp_pos, pos);
 
-	if (chi_move_from (*m) == 3) {
-	    bitv64 from_mask = ((bitv64) 1) << 3;
-	    int to = chi_move_to (*m);
+		if (chi_move_from(*m) == 3) {
+			bitv64 from_mask = ((bitv64) 1) << 3;
+			int to = chi_move_to(*m);
 
-	    if ((to == 5 || to == 1) && from_mask & pos->w_kings) {
-		int tmp_to = (3 + to) >> 1;
+		if ((to == 5 || to == 1) && from_mask & pos->w_kings) {
+			int tmp_to = (3 + to) >> 1;
 
-		if (chi_check_check (pos))
-		    continue;
+		if (chi_check_check(pos))
+			continue;
 
-		chi_move_set_to (*m, tmp_to);
-		chi_make_move (&tmp_pos, *m);
-		if (chi_check_check (&tmp_pos))
-		    continue;
-		chi_copy_pos (&tmp_pos, pos);
-		chi_move_set_to (*m, to);
-	    }
-	} else if (chi_move_from (*m) == 59) {
-	    bitv64 from_mask = ((bitv64) 1) << 59;
-	    int to = chi_move_to (*m);
+		chi_move_set_to(*m, tmp_to);
+		chi_make_move(&tmp_pos, *m);
+		if (chi_check_check(&tmp_pos))
+			continue;
+		chi_copy_pos(&tmp_pos, pos);
+		chi_move_set_to(*m, to);
+		}
+	} else if (chi_move_from(*m) == 59) {
+		bitv64 from_mask = ((bitv64) 1) << 59;
+		int to = chi_move_to(*m);
 
-	    if ((to == 61 || to == 57) && from_mask & pos->b_kings) {
-		int tmp_to = (59 + to) >> 1;
+		if ((to == 61 || to == 57) && from_mask & pos->b_kings) {
+			int tmp_to = (59 + to) >> 1;
 
-		if (chi_check_check (pos))
-		    continue;
+			if (chi_check_check(pos))
+				continue;
 
-		chi_move_set_to (*m, tmp_to);
-		chi_make_move (&tmp_pos, *m);
-		if (chi_check_check (&tmp_pos))
-		    continue;
-		chi_copy_pos (&tmp_pos, pos);
-		chi_move_set_to (*m, to);
-	    }
+			chi_move_set_to(*m, tmp_to);
+			chi_make_move(&tmp_pos, *m);
+			if (chi_check_check(&tmp_pos))
+				continue;
+			chi_copy_pos(&tmp_pos, pos);
+			chi_move_set_to(*m, to);
+		}
 	}
 
-	chi_make_move (&tmp_pos, *m);
-	if (chi_check_check (&tmp_pos))
-	    continue;
+	chi_make_move(&tmp_pos, *m);
+	if (chi_check_check(&tmp_pos))
+		continue;
 
-	*move_stack++ = *m;
-    }
+		*move_stack++ = *m;
+	}
 
-    return move_stack;
+	return move_stack;
 }
 
-/*
-Local Variables:
-mode: c
-c-style: K&R
-c-basic-shift: 8
-End:
-*/
