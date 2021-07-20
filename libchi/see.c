38902f9e42a95ed6de63000b6e32f654e45be244
Guido Flohr <guido.flohr@cantanea.com>
2021-07-20 23:40:58 +0300

    (length) explanation for the ep bit fiddling

--- libchi/see.c
+++ libchi/see.c
@@ -34,17 +34,50 @@ chi_obvious_attackers(const chi_pos *pos, chi_move mv,
 	bitv64 not_from_mask = ~(1ULL << chi_move_from(mv));
 	bitv64 mask;
 	/* In case of an en passant hit, we have to remove the victim from the
-	 * occupancy because it obscures parts of its file.  The vicim is
-	 * EP_TO_OFFSETS away from the actual TO field depending on which site
+	 * occupancy because it obscures parts of its file.  The victim is
+	 * EP_TO_OFFSETS away from the actual TO square depending on which site
 	 * is on move (does the hit).
-	 * 
+	 *
 	 * The funny bit fiddling with the ep flag is there to avoid a
-	 * a multiplication and a branch.
+	 * a multiplication and a branch:
+	 *
+	 * If a pawn is hit en passant, it is either -8 or +8 squares "away"
+	 * from where it gets hit, depending on whether it is a white or black
+	 * pawn.  This is defined in EP_TO_OFFSETS.  And because chi_on_move()
+	 * extracts just one single bit from a CHI_POS, that is either 0 or 1,
+	 * it can be used as an index into EP_TO_OFFSETS.
+	 *
+	 * Therefore, TO + this offset is the index of the piece/pawn to remove
+	 * from the (bit)board, and shifting 1ULL index bits to the left turns
+	 * that into a 1-bit mask for the square of the piece/pawn to be removed.
+	 *
+	 * CHI_MOVE_IS_EP(MV) isolates the en-passant bit of the position, it is
+	 * defined as MV & 0x1000, in other words a 1 shifted left by 12 where
+	 * 12 is CHI_MOVE_EP_OFFSET.  This is undone here (by right-shift) so
+	 * that the flag becomes a boolean, either 0 or 1.  Multiplying it with
+	 * -1 turns it either into 0 or an 64 bit integer with all bits set.
+	 *
+	 * This 64 bit integer is now ANDed with 1-bit mask obtained above, for
+	 * the target square of the pawn that did the double step.  The result of
+	 * this operation is either 0 if en passant is not possible for the current
+	 * position (the most likely case) or it is a bitboard where all bits are
+	 * set except for the one that represents the target square of the pawn's
+	 * double step.
+	 *
+	 * This is now bitwisely removed from the ORed masks of the white and
+	 * black pieces, so that OCCUPANCY now represents all pieces that are
+	 * effectively on the board.
+	 *
+	 * Why all that hassle? For a normal capture, the position of the victim
+	 * is ignored.  Its place is taken by all pieces taking part in the
+	 * capture sequence.  But for en passant hits, the pawn that just did
+	 * the double step has to be removed because it may obscure sliding
+	 * pieces that can attack the en passant square.
 	 */
 	int ep_to_offsets[2] = { -8, +8 };
 	bitv64 occupancy = (pos->w_pieces | pos->b_pieces)
 		& ~(-(chi_move_is_ep(mv) >> chi_move_ep_offset)
-		    & (1 << (to + ep_to_offsets[chi_on_move(pos)])));
+		    & (1ULL << (to + ep_to_offsets[chi_on_move(pos)])));
 	bitv64 bishop_mask = Bmagic(to, occupancy) & not_from_mask;
 	bitv64 rook_mask = Rmagic(to, occupancy) & not_from_mask;
 	bitv64 queen_mask = bishop_mask | rook_mask;
